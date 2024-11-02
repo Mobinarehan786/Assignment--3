9a59ec7397cc8318a75b9f553853b131ec5a638a
mobinakhatoonmk@gmail.com <mobinakhatoonmk@gmail.com>
2024-11-02 23:02:29 +0500

    update

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -7,7 +7,7 @@
    <div> 
     <p className="text"> 
    Hi I am <strong>Mobina Khatoon</strong>, a passionate frontend developer skilled in creating responsive, dynamic,<br></br> and visually appealing websites. With expertise in HTML, CSS, JavaScript,
-    and modern frameworks,<br></br> I turn ideas into functional and user friendly digital experiences. Lets build something beautiful together"</p>
+    and modern frameworks,<br></br> I turn ideas into functional and user friendly digital experiences. Lets build something beautiful together</p>
   
 
 
